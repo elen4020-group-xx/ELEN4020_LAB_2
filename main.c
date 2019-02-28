dfd2e45a8af1bb39614bc611cbee234a41d95c3d
Junaid-D <1094837@students.wits.ac.za>
2019-02-28 14:24:48 +0000

    size 4 block alg

--- main.c
+++ main.c
@@ -5,12 +5,10 @@
 
 void swap(int* i1,int* i2)
 {
-	printf("%i\n",*i1 );
-
 	 int temp =*i2;
 	 *i2=*i1;
 	 *i1=temp;
-	printf("%i\n\n", *i1 );
+	
 }
 
 void naiveTranspose(rank2Tensor* t)
@@ -38,17 +36,52 @@ void DiagTranspose( rank2Tensor* t){
 	}
 
 }
+
+void blockTranspose(rank2Tensor* t)
+{
+
+	for(int i=0; i<t->rows; i+=2)
+	{
+
+
+
+		for(int j=i; j<t->cols; j+=2)
+		{
+			//internal transpose
+			swap(&(t->matrix[i][j+1]),&(t->matrix[i+1][j]));
+			//swap(&(t->matrix[j][i+1]),&(t->matrix[j+1][i]));
+
+
+
+			if(i!=j)
+			{
+				swap(&(t->matrix[j][i+1]),&(t->matrix[j+1][i]));
+				swap(&(t->matrix[i][j]),&(t->matrix[j][i]));
+				swap(&(t->matrix[i][j+1]),&(t->matrix[j][i+1]));		
+				swap(&(t->matrix[i+1][j]),&(t->matrix[j+1][i]));
+				swap(&(t->matrix[i+1][j+1]),&(t->matrix[j+1][i+1]));	
+			}
+
+			///
+			
+		}
+
+
+
+	}
+}
+
 int main ()
 {
 	srand(time(NULL));
 	rank2Tensor t;
 
-	t.rows=3;
-	t.cols=3;
+	t.rows=4;
+	t.cols=4;
 	initRank2Tensor(&t);
 
 	displayRank2Tensor(&t);
-	naiveTranspose(&t);
+	blockTranspose(&t);
 
 	printf("\n");
 	displayRank2Tensor(&t);
