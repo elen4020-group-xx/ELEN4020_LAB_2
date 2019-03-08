4168028542f5992ecf9a0b4b1f1c60bd5056a81a
Junaid <1094837@students.wits.ac.za>
2019-03-08 08:26:44 +0200

    added compile and run script

---
+++ compile_and_run.sh
@@ -0,0 +1,12 @@
+rm -f *.o
+echo "compiling"
+gcc Omp.c -o omp.o -fopenmp
+gcc Pthreads.c -o pt.o -lpthread -fopenmp
+gcc Serial.c -o ser.o -fopenmp
+echo "done compiling"
+echo "OMP Version"
+./omp.o
+echo "PThreads Version"
+./pt.o
+echo "Serial Version"
+./ser.o
